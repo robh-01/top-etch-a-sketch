7582686d9012795328555916a892ac3d67ab1fc9
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2024-07-24 15:20:01 +0545

    Add a functionality that paints RGB randomly

--- script.js
+++ script.js
@@ -7,8 +7,35 @@ for (i = 0; i < dimension; i++) {
     rowContainer.classList.add('rowContainer');
     for (j = 0; j < dimension; j++) {
         let box = document.createElement('div');
+        box.classList.add('box')
         box.style.cssText =
             "border: 1px solid black; flex: 1 1 auto;";
         rowContainer.appendChild(box);
     }
 }
+
+
+
+
+let boxList = document.querySelectorAll('.box');
+boxList.forEach((box) => {
+    box.addEventListener('mouseenter', function paint(e) {
+        if (e.target.classList.contains('red') || e.target.classList.contains('green') || e.target.classList.contains('blue')) {
+
+        }
+        else {
+            let color = Math.floor(Math.random() * 3) + 1;
+            switch (color) {
+                case 1:
+                    e.target.classList.add('red');
+                    break;
+                case 2:
+                    e.target.classList.add('green');
+                    break;
+                default:
+                    e.target.classList.add('blue');
+            }
+        }
+
+    });
+})
